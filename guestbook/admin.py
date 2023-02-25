3602a7abbe29a7c1d3da3be865bc6398fbed525d
Daniyar Otegen <daniyarotegen@gmail.com>
2023-02-25 11:55:48 +0600

    Создал модель для записи, зарегал в админке, сделал миграции

--- guestbook/admin.py
+++ guestbook/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from guestbook.models import Record
 
-# Register your models here.
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'text', 'created_at', 'updated_at', 'status')
+    list_filter = ('name', 'email', 'status', 'created_at', 'updated_at')
+    search_fields = ('name', 'status')
+    fields = ('name', 'email', 'text', 'created_at', 'updated_at', 'status')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(Record, RecordAdmin)
--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import TextChoices
 
-# Create your models here.
+
+class StatusChoice(TextChoices):
+    ACTIVE = 'ACTIVE', 'Active'
+    BLOCKED = 'BLOCKED', 'Blocked'
+
+
+class Record(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Name')
+    email = models.EmailField(max_length=100, null=False, blank=False, verbose_name='Email')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+    status = models.CharField(
+        max_length=20, choices=StatusChoice.choices, default=StatusChoice.ACTIVE, verbose_name='Status'
+    )
+
+    def __str__(self):
+        return self.name
