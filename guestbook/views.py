0a2fad9052cd3a7ac14528527dab5183e186d3b1
Daniyar Otegen <daniyarotegen@gmail.com>
2023-02-25 13:04:07 +0600

    Добавил вью, шаблон и ссылку для вывода индексной страницы сортируя по дате создания (сначала свежее) и исключил показ с заблоченным статусом

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,3 +1,11 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
+from guestbook.models import Record, StatusChoice
 
-# Create your views here.
+
+def index_view(request: WSGIRequest):
+    records = Record.objects.exclude(status=StatusChoice.BLOCKED).order_by('-created_at')
+    context = {
+        'records': records
+    }
+    return render(request, 'index.html', context=context)
